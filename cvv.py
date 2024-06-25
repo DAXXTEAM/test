a6ad63787c4375b040c2b77cfdcf2f9f5580546f
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-06-26 01:17:22 +0530

    Update cvv.py

--- cvv.py
+++ cvv.py
@@ -42,9 +42,10 @@ async def check_cc(cc, amount, currency, bot, chat_id, username, vip_status, ret
         await bot.send_message(chat_id=chat_id, text=response_message, parse_mode='HTML', disable_web_page_preview=True)
 
     except requests.exceptions.RequestException as e:
-        await bot.send_message(chat_id=chat_id, text=f"{cc} Request Exception: {str(e)}. Retrying...")
-        time.sleep(1)
-        await check_cc(cc, amount, currency, bot, chat_id, username, vip_status, retry_count + 1)
+        if retry_count < MAX_RETRIES:
+            await bot.send_message(chat_id=chat_id, text=f"{cc} Request Exception: {str(e)}. Retrying...")
+            time.sleep(1)
+            await check_cc(cc, amount, currency, bot, chat_id, username, vip_status, retry_count + 1)
 
     except json.JSONDecodeError as e:
         await bot.send_message(chat_id=chat_id, text=f"{cc} JSON Decode Error: {str(e)}")
@@ -57,30 +58,6 @@ def is_vip(user_id):
     with open("vip.txt", "r") as file:
         vip_users = file.readlines()
     return str(user_id) + "\n" in vip_users
-
-async def xvv(update: Update, context: CallbackContext):
-    try:
-        cc = context.args[0]
-        parts = cc.split('|')
-        if len(parts[2]) == 2:
-            parts[2] = '20' + parts[2]
-        formatted_cc = ':'.join(parts)
-    except IndexError:
-        await update.message.reply_text(f"{update.effective_user.username} pls provide cc")
-        return
-    except Exception:
-        await update.message.reply_text(f"{update.effective_user.username} why providing wrong information")
-        return
-
-    amount = 3.0
-    currency = "usd"
-    bot = context.bot
-    chat_id = update.effective_chat.id
-    username = update.effective_user.username
-    user_id = update.effective_user.id
-    vip_status = "OWNER" if is_vip(user_id) else "FREE"
-
-    await check_cc(formatted_cc, amount, currency, bot, chat_id, username, vip_status)
 
 async def process_cards(update: Update, context: CallbackContext):
     document = update.message.document
@@ -111,11 +88,8 @@ async def process_cards(update: Update, context: CallbackContext):
             await check_cc(cc.replace('|', ':'), amount, currency, bot, chat_id, username, vip_status)
 
 def main():
-    bot_token = "7266886772:AAFbSsQc2EiIXZ5XBoU2H7m9ci6mevp7LKQ"
+    bot_token = "7266886772:AAEhY-yWQSEu7mxMWkJCJyesK_qgqPZlPks"
     application = Application.builder().token(bot_token).build()
-
-    # Handle command for text input
-    application.add_handler(CommandHandler("xvv", xvv))
 
     # Handle command for file input
     application.add_handler(MessageHandler(filters.Document.MimeType("text/plain") & filters.Command(".CVV"), process_cards))
